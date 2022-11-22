6a0f0702099f1f8df8b8b6c00a310aaf3b24f627
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-22 11:31:16 +0100

    mysql test- fix error

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- Prepare a MySQL server for test
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
