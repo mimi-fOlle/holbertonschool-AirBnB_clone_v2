abeea4870f792c1fc4d16ed5ae753317511e6d7f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-23 17:50:22 +0100

    modify some part in do_create

--- console.py
+++ console.py
@@ -33,7 +33,7 @@ class HBNBCommand(cmd.Cmd):
     def preloop(self):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb)')
+            print('(hbnb) ', end='')
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
@@ -118,7 +118,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """ Create an object of any class"""
         new = args.split(" ")
-        if not new:
+        if not args:
             print("** class name missing **")
             return
         elif new[0] not in HBNBCommand.classes:
@@ -138,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
 
                 else:
                     first[1] = int(first[1])
-                    setattr(new_instance, first[0], first[1])
+                setattr(new_instance, first[0], first[1])
             
             except (Exception):
                 continue
