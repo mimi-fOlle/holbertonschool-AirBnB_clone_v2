2e13ea4f3d4ea9665d956723d9b99cbb85a01578
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-23 16:51:55 +0100

    add getenv and fix indentation

--- models/state.py
+++ models/state.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """Getter of cities"""
-        from models import storage
-        list_city = []
-        for city in storage.all(City):
-            if self.id == city.state_id:
-                list_city.append(city)
-        return list_city 
+        @property
+        def cities(self):
+            """Getter of cities"""
+            from models import storage
+            list_city = []
+            for city in storage.all(City):
+                if self.id == city.state_id:
+                    list_city.append(city)
+            return list_city 
