a2a4b11278df5c7c27b12aa5646694c4c1b2d383
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-23 14:26:36 +0100

    update city, state and __init__

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Getter of cities"""
+        list_city = []
+        for city in storage.all(City):
+            if self.id == city.state_id:
+                list_city.append(city)
+        return list_city 
