315a7a265d3a86b0787a50a8da62c1b109dc640a
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-24 13:22:07 +0100

    modify file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,14 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return FileStorage.__objects
-        else:
+        if cls is not None:
             new = {}
             for key, value in self.__objects.items():
                 if isinstance(value, cls):
                     new[key] = value
             return new
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -60,7 +59,8 @@ class FileStorage:
         """Deletes obj from __objects"""
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-        return
+        else:
+            return
         if key in self.__objects:
             del self.__objects[key]
             self.save()
