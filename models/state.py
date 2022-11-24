cb5d96671ad14c32e7205887123e14e75c57547c
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-24 12:58:37 +0100

    modify state

--- models/state.py
+++ models/state.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
 from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """Getter of cities"""
-        from models import storage
-        list_city = []
-        for city in storage.all(City):
-            if self.id == city.state_id:
-                list_city.append(city)
-        return list_city 
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        @property
+        def cities(self):
+            """Getter of cities"""
+            from models import storage
+            from models.city import City
+            list_city = []
+            for city in storage.all(City):
+                if self.id == city.state_id:
+                    list_city.append(city)
+            return list_city 
