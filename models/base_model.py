14f964502d4453e749df30b85a1d26d5f81fafdd
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-24 19:02:02 +0100

    modified base_model and state

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
--- models/state.py
+++ models/state.py
@@ -10,7 +10,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete")
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         @property
