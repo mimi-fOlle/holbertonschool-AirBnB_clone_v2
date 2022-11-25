864b65c2e5bad6971230579ec678382d711faf50
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-25 20:24:00 +0100

    origin file

--- models/place.py
+++ models/place.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
-from sqlalchemy.orm import relationship
+#from sqlalchemy import Column, String, ForeignKey
+#from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
@@ -14,16 +11,16 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForeignKey="cities.id")
-    user_id = Column(String(60), nullable=False, ForeignKey="users.id")
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Integer, nullable=False, default=0.0)
-    longitude = Column(Integer, nullable=False, default=0.0)
+#    __tablename__ = 'places'
+#    city_id = Column(String(60), nullable=False, ForeignKey="cities.id")
+#    user_id = Column(String(60), nullable=False, ForeignKey="users.id")
+#    name = Column(String(128), nullable=False)
+#    description = Column(String(1024), nullable=True)
+#    number_rooms = Column(Integer, nullable=False, default=0)
+#    number_bathrooms = Column(Integer, nullable=False, default=0)
+#    max_guest = Column(Integer, nullable=False, default=0)
+#    price_by_night = Column(Integer, nullable=False, default=0)
+#    latitude = Column(Integer, nullable=False, default=0.0)
+#    longitude = Column(Integer, nullable=False, default=0.0)
     amenity_ids = []
 
