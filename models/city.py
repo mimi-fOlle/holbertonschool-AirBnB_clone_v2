46c1c3db485f198de5ef5aaaefdbc95f96ec175f
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-25 20:25:52 +0100

    update

--- models/city.py
+++ models/city.py
@@ -12,6 +12,6 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"),
                       nullable=False)
 
-    places = relationship("Place", backref="cities", cascade="all, delete", 
-                          passive_deletes=True)
+#    places = relationship("Place", backref="cities", cascade="all, delete", 
+#                          passive_deletes=True)
     
