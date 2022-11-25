0c3c55b6691c27ad79e7b1b4addbcb42d7b2e672
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-25 16:15:15 +0100

    remove

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
+    city_id ="" 
     user_id = ""
     name = ""
     description = ""
