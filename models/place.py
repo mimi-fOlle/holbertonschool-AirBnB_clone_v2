dfa56b15a0754ca04fdaefa55deab624e19f359b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-25 16:26:09 +0100

    update

--- models/place.py
+++ models/place.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+#from sqlalchemy import Column, String, ForeignKey
+#from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """ A place to stay """
-<<<<<<< HEAD
     city_id ="" 
     user_id = ""
     name = ""
     description = ""
-=======
     __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForeignKey="cities.id")
-    user_id = Column(String(60), nullable=False, ForeignKey="users.id")
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
->>>>>>> 7ce36579d602c381ef1ea8d4875973bf586efabd
+#    city_id = Column(String(60), nullable=False, ForeignKey="cities.id")
+#    user_id = Column(String(60), nullable=False, ForeignKey="users.id")
+#    name = Column(String(128), nullable=False)
+#    description = Column(String(1024), nullable=True)
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
