d33f84bb456350ca696f99e99b696ee41858db6c
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-28 10:18:32 +0100

    route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module web_flask"""
+from flask import Flask
+from os import getenv
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Starts a Flask web application"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    port = int(getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug = True)
