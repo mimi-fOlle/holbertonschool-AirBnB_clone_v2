df98155846036576e3f05704f22d8619bcf34756
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-28 13:44:58 +0100

    nmb odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module web_flask"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{:d} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
