e13befe72f1cecac9f674a94470b383ff0c316d2
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-28 11:17:34 +0100

    value of text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module web_flask"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Starts a Flask web app"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Binds with hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Display "C" followed by the value of the text variable"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """Display "Python" followed by the value of the text variable"""
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host=0.0.0.0, port=5000)
