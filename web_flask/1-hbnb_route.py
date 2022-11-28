4dbec695bd6cd21b04846c333b6ff2fcf9ef8c85
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-28 10:39:18 +0100

    fix pycostyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,12 @@ def hello():
     """Starts a Flask web app and listening on 0.0.0.0, port 5000"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Bind to hbnb"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
