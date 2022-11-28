c37ade8f96ef249156b4ba73c2f981dd4311ada3
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-28 11:43:48 +0100

    numberint:n

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,10 +25,9 @@ def python(text="is cool"):
     return "Python " + text.replace("_", " ")
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    if int(n):
-        return "{:d}".format(n)
+    return "{:d} is a number".format(n)
 
 
 if __name__ == "__main__":
