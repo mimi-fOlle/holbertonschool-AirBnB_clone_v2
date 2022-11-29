7cd1d560b90703be7a2dd51ae2dbc7085558f4d3
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-29 10:32:37 +0100

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,22 +3,43 @@
 from flask import Flask, render_template, request
 from models import storage
 from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+#@app.route("/cities_by_states")
+#def states_template():
+#    states = list(storage.all(State).values())
+#    states.sort(key=lambda state: state.name)
+#    return render_template("8-cities_by_states.html", states=states)
+
+@app.route('/states_list')
 def states_list():
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
+    '''Render states list'''
+    # Get list of states
+    states = []
+    data = storage.all(State)
+    for key, value in data.items():
+        states.append(value)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    cities = list(storage.all(City).values())
-    cities.sort(key=lambda city: city.name)
-    return render_template("8-cities_by_states.html", cities=cities)
+@app.route('/cities_by_states')
+def cities_state():
+    '''Render cities by state list'''
+    # Get list of states
+    states = []
+    data = storage.all(State)
+    for key, value in data.items():
+        states.append(value)
 
+    # Get list of cities
+    cities = []
+    data = storage.all(City)
+    for key, value in data.items():
+        cities.append(value)
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -26,4 +47,5 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port=5000, debug=True)
