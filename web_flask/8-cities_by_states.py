2c52fddd10860fb2701ef94b1e3bacbb2390ce1b
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-11-29 11:08:56 +0100

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,14 +3,32 @@
 from flask import Flask, render_template, request
 from models import storage
 from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
+@app.route("/states_list")
+def states_list():
+    states = []
+    data = storage.all(State)
+    for key, value in data.items():
+        states.append(value)
+    return render_template("7-states_list.html", states=states)
+
+
 @app.route("/cities_by_states")
-def states_template():
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template("8-cities_by_states.html", states=states)
+def cities_by_states():
+    states = []
+    data = storage.all(State)
+    for key, value in data.items():
+        states.append(value)
+
+    cities = []
+    data = storage.all(City)
+    for key, value in data.items():
+        cities.append(value)
+    return render_template("8-cities_by_states.html", states=states,
+                           cities=cities)
 
 
 @app.teardown_appcontext
